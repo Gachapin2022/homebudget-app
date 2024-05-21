666c89b34877c8a727347aa801d5db98e811b53d
take <take030615@outlook.com>
2024-05-21 13:52:59 +0900

    index

---
+++ resources/views/homebudget/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <header>
+        <h1>{{ config('app.name') }}</h1>
+    </header>
+    <section class="container">
+        <div class="balance">
+            <h3>支出一覧</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>日付</th>
+                        <th>カテゴリ</th>
+                        <th>金額</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- 支出データのループ処理 -->
+                </tbody>
+            </table>
+        </div><!-- /balance -->
+
+        <div class="add-balance">
+            <h3>支出の追加</h3>
+
+            <form action="#" method="POST">
+                <label for="date">日付：</label>
+                <input type="date" id="date" name="date">
+
+                <label for="category">カテゴリ：</label>
+                <select name="category" id="category">
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+
+                <label for="price">金額：</label>
+                <input type="text" id="price" name="price">
+
+                <button type="submit">追加</button>
+            </form>
+        </div><!-- /add-balance -->
+    </section><!-- /container -->
+</body>
+</html>
